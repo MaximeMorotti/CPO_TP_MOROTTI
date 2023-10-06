37519c7fb83fb16775111240bb68d96b2a075293
33768 <33768@Maxchine.education.lakanal.epf.fr>
2023-10-06 18:51:32 +0200

    Création d'un nouveau projet versionné

---
+++ TP2_manip_MOROTTI/src/tp2_manip_morotti/Tartiflette.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_manip_morotti;
+
+/**
+ *
+ * @author 33768
+ */
+public class Tartiflette {
+    int nbcalories;
+    public Tartiflette (int calo){
+        nbcalories = calo;
+    }
+}
