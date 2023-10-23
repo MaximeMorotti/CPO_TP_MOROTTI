8af83b239cb6de810c6377f111e9f4a7304cff3a
33768 <33768@Maxchine.education.lakanal.epf.fr>
2023-10-23 15:35:23 +0200

    Création d'un nouveau projet versionné

--- TP3_Heroic_Fantasy_MOROTTI/src/Personages/Personnage.java
+++ TP3_Heroic_Fantasy_MOROTTI/src/Personages/Personnage.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Personages;
+import Armes.Arme;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,9 +13,13 @@ package Personages;
 public abstract class Personnage {
     String Name;
     int Hp;
+    ArrayList<Arme> sakado = new ArrayList<Arme>();
+    Arme Arme_en_Main;
+    
     public Personnage (String nom, int Pv){
         Name = nom;
         Hp = Pv;
+        Arme_en_Main = null;
     }
     
     public int getHp(){
@@ -22,11 +28,42 @@ public abstract class Personnage {
     public String getName(){
         return this.Name;
     }
+    public void getweapon(Arme weap){
+        if (sakado.size() >=5){
+            System.out.println("Mon sac est fait!!!");
+        }
+        else{
+            sakado.add(weap);
+            if (sakado.size() == 1){
+                Arme_en_Main = weap;
+            }
+        }
+    }
+    
+    public void changeArme_en_Main(String nvarme){
+        for(int i = 0 ; i >= this.sakado.size() ; i++){
+            if (this.sakado.get(i).getName() == nvarme ){
+                Arme_en_Main = this.sakado.get(i);
+                System.out.println("arme équiper.");
+            }
+        if (Arme_en_Main != this.sakado.get(i) ){
+            System.out.println("arme non posséder");
+           
+        }
+        
+            
+        }
+        
+    }
+
+    public Arme getArme_en_Main() {
+        return Arme_en_Main;
+    }
     
     @Override
     public String toString() {
         String chaine_a_retourner;
-        chaine_a_retourner = "Nom du personnage: " + Name + " Points de vie du personnage: " + Hp ;
+        chaine_a_retourner = "Nom du personnage: " + Name + " Points de vie du personnage: " + Hp "\nArme de l' inventaire: " + sakado + "\n arme équiper: " + Arme_en_Main;
         return chaine_a_retourner ;
     }
 }
