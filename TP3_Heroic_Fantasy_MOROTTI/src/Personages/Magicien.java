c6ea1cd0970e6aa540ca8dc7fdda64b1e33df6eb
33768 <33768@Maxchine.education.lakanal.epf.fr>
2023-10-24 13:52:25 +0200

    Création d'un nouveau projet versionné

--- TP3_Heroic_Fantasy_MOROTTI/src/Personages/Magicien.java
+++ TP3_Heroic_Fantasy_MOROTTI/src/Personages/Magicien.java
@@ -4,19 +4,30 @@
  */
 package Personages;
 
+import tp3_heroic_fantasy_morotti.etreVivant;
+
 /**
  *
  * @author 33768
  */
-public class Magicien extends Personnage {
-    
+public class Magicien extends Personnage implements etreVivant{
+    int nb_mag=0;
     boolean Validée_par_la_street;
     
     public Magicien(String nom, int Pv, boolean diplome) {
         super(nom, Pv);
+        nb_mag++;
         Validée_par_la_street =diplome;
-        
     }
+    
+    public void finalize(){
+        nb_mag = nb_mag-1;
+    }
+    
+    public int attaque(){
+        return 20;
+    }
+    
     public void modif_Qalification(){
         
         if (Validée_par_la_street==true){
