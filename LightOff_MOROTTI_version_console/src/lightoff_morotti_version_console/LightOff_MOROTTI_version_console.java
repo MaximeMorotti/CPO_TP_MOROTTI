f846aab6699ff2f97d0dee7111064a6bc237409b
33768 <33768@Maxchine.education.lakanal.epf.fr>
2023-10-25 11:54:16 +0200

    Création d'un nouveau projet versionné

---
+++ LightOff_MOROTTI_version_console/src/lightoff_morotti_version_console/LightOff_MOROTTI_version_console.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lightoff_morotti_version_console;
+
+/**
+ *
+ * @author 33768
+ * Nom:Morotti
+ * Group:TDC
+ * rôle: Console Miniprojet 
+ * Date:25/10/2023
+ * 
+ */
+public class LightOff_MOROTTI_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ LightOff_MOROTTI_version_console/src/lightoff_morotti_version_console/Partie.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lightoff_morotti_version_console;
+
+/**
+ *
+ * @author 33768
+ * Nom:Morotti
+ * Group:TDC
+ * rôle: Partie Miniprojet 
+ * Date:25/10/2023
+ * 
+ */
+public class Partie {
+    
+}
