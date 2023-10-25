fb6ca33bab86f9290ef1efe2b174ee19c608fb0a
33768 <33768@Maxchine.education.lakanal.epf.fr>
2023-10-25 18:31:40 +0200

    Création d'un nouveau projet versionné

--- LightOff_MOROTTI_version_console/src/lightoff_morotti_version_console/GrilleDeCellules.java
+++ LightOff_MOROTTI_version_console/src/lightoff_morotti_version_console/GrilleDeCellules.java
@@ -8,14 +8,11 @@ import lightoff_morotti_version_console.CelluleLumineuse;
 
 /**
  *
- * @author 33768
- * Nom:Morotti
- * Group:TDC
- * rôle: Grille Miniprojet 
- * Date:25/10/2023
- * 
+ * @author 33768 Nom:Morotti Group:TDC rôle: Grille Miniprojet Date:25/10/2023
+ *
  */
 public class GrilleDeCellules {
+
     Random r = new Random();
     int nbLignes;
     int nbColonnes;
@@ -23,6 +20,7 @@ public class GrilleDeCellules {
 
     /**
      * Méthode permettant de crée un tableau remplie de cellulue luineuse.
+     *
      * @param nbLignes Permet d' initialiser le nombre de ligne du tableau.
      * @param nbColonnes Permet d' initialiser le nombre de colone du tableau.
      */
@@ -35,95 +33,171 @@ public class GrilleDeCellules {
                 matriceCellules[i][j] = new CelluleLumineuse();
             }
         }
-                
+
     }
-    
+
     /**
-     * Méthode permetant modifier l'état de tout les cellule 
-     * en les passent en éteinte ( etat = false).
+     * Méthode permetant modifier l'état de tout les cellule en les passent en
+     * éteinte ( etat = false).
      */
-    public void eteindreToutesLesCellule (){
+    public void eteindreToutesLesCellule() {
         for (int i = 0; i < nbLignes; i++) {
             for (int j = 0; j < nbColonnes; j++) {
                 matriceCellules[i][j].eteindreCellule();
             }
         }
     }
-    
+
     /**
-     * Méthode qui allume une ligne ou une colode ou une diagonalle alléatoirement.
+     * Méthode qui allume une ligne ou une colode ou une diagonalle
+     * alléatoirement.
      */
-    public void  activerLigneColonneOuDiagonaleAleatoire(){
+    public void activerLigneColonneOuDiagonaleAleatoire() {
         int Possibilité;
         int Ln;
         int Cl;
         int Dg;
         Possibilité = r.nextInt(3);
-        if (Possibilité == 0){
+        if (Possibilité == 0) {
             Ln = r.nextInt(nbLignes);
             for (int j = 0; j < nbColonnes; j++) {
                 matriceCellules[Ln][j].activerCellule();
             }
         }
-        if (Possibilité == 1){
+        if (Possibilité == 1) {
             Cl = r.nextInt(nbColonnes);
-                    for (int i = 0; i < nbLignes; i++) {
+            for (int i = 0; i < nbLignes; i++) {
                 matriceCellules[i][Cl].activerCellule();
             }
         }
-        if (Possibilité == 2){
+        if (Possibilité == 2) {
             Dg = r.nextInt(2);
-            if (Dg == 0){
+            if (Dg == 0) {
                 for (int i = 0; i < nbLignes; i++) {
                     for (int j = 0; j < nbColonnes; j++) {
-                        if (i == j){
+                        if (i == j) {
                             matriceCellules[i][j].activerCellule();
                         }
-                
                     }
-           
                 }
-            
-            }
-            else {
+
+            } else {
                 for (int i = 0; i < nbLignes; i++) {
                     for (int j = 0; j < nbColonnes; j++) {
-                        if (nbLignes - i == j){
+                        if (nbLignes - i == j) {
                             matriceCellules[i][j].activerCellule();
                         }
-                
                     }
-           
                 }
             }
-                
-            }
-        
-        
+
+        }
+
     }
+
+    /**
+     * Méthode permettant dengénérer un tableau de Cellule lumineuse mélanger au
+     * nbTours fois choisie.
+     *
+     * @param nbTours nombre choisie de mélange fait sur la grille.
+     */
+    public void melangerMatriceAleatoirement(int nbTours) {
+        GrilleDeCellules GrCl = new GrilleDeCellules(6, 6);
+        GrCl.eteindreToutesLesCellule();
+        for (int i = 0; i < nbTours; i++) {
+            GrCl.activerLigneColonneOuDiagonaleAleatoire();
+        }
+    }
+
+    /**
+     * Méthode qui active tout une ligne donnée.
+     * @param idLigne ligne a activée.
+     */
+    public void activerLigneDeCellules(int idLigne) {
+        for (int j = 0; j < nbColonnes; j++) {
+            matriceCellules[idLigne][j].activerCellule();
+
+        }
+    }    
+    
+    /**
+     * Méthode qui active tout une colone donnée.
+     * @param idColonne colanne a activé.
+     */
+    public void activerColonneDeCellules(int idColonne) {
+        for (int i = 0; i < nbLignes; i++) {
+            matriceCellules[i][idColonne].activerCellule();
+        }
+    }
+    
+    /**
+     * Méthode qui active tout la Diagonale Descendante.
+     */
+    public void activerDiagonaleDescendante() {
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                if (i == j) {
+                    matriceCellules[i][j].activerCellule();
+                }
+            }
+        }  
+    }
+    
+    /**
+     * Méthode qui active tout la Diagonale Montante.
+     */
+    public void activerDiagonaleMontante() {
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                if (nbLignes - i == j) {
+                    matriceCellules[i][j].activerCellule();
+                }
+            }
+        }
+    }
+    
+    /**
+     * Méthode permettant de vériffier si tout les cellule du tableau sont éteintes.
+     * @return true si elles sont bien tout éteintes sinon false.
+     */
+    public boolean cellulesToutesEteintes(){
+        boolean éteint = true;
+        for (int i = 0; i < nbLignes; i++) {
+            for (int j = 0; j < nbColonnes; j++) {
+                if ( matriceCellules[i][j].estEteint() == false){
+                    éteint = false;
+                }
+            }
+        }
+        return éteint;
+    }
+    
+    /**
+     * Méthode permettant d' adapter l' affichage de la cellule selon son état.
+     * @return X si allumer O si éteinte.
+     */
+    @Override
+    public String toString() {
+        String chaine_a_retourner;
+        String L1 = " | ";
+        String espassement = "";
+        String LnGr = "";
+        for (int j = 0; j < nbColonnes; j++) {
+            L1 += j + " | ";
+        }
+        for (int i = 0; i < L1.length(); i++) {
+            espassement += "-";
+        }
+        for (int j = 0; j < nbColonnes; j++) {
+            L1 += j + " | ";
+        }
+        return chaine_a_retourner ;
+    }
+    
 }
 """
 
 
-
-- La méthode activerLigneColonneOuDiagonaleAleatoire() active de manière aléatoire
-soit une ligne, soit une colonne, soit une diagonale de cellules dans la grille. La logique
-de l'activation est basée sur un nombre aléatoire.
-- La méthode melangerMatriceAleatoirement(int nbTours) permet de générer un
-plateau de cellules lumineuses de manière aléatoire à partir d’un nombre spécifié de
-tours. Partant d’une matrice existante – c.-à-d. la matriceCellules stockée en tant
-qu’attribut - cette méthode commence par éteindre toutes les cellules, et ensuite, pour
-chaque tour, active de manière aléatoire une ligne, une colonne ou une diagonale de
-cellules de la matrice. Générer une grille ainsi permet de s’assurer que la grille dispose
-bien d’une solution réalisable.
-- Les méthodes activerLigneDeCellules(int idLigne) et activerColonneDeCellules(int
-idColonne) activent toutes les cellules d'une ligne ou d'une colonne spécifique de la
-grille, dont le numéro est passé en paramètre.
-- Les méthodes activerDiagonaleDescendante() et activerDiagonaleMontante()
-activent respectivement la diagonale descendante et la diagonale montante de la grille
-en allumant les cellules correspondantes.
-- La méthode cellulesToutesEteintes() vérifie si toutes les cellules de la grille sont
-éteintes. Elle retourne true si toutes les cellules sont éteintes, et false sinon.
 - Enfin, la méthode toString() est redéfinie pour afficher l'état de la grille sous forme
 d'une représentation visuelle organisée. Elle crée une chaîne de caractères avec des
 indices pour les lignes et les colonnes, ainsi que des caractères "X" et "O" pour
